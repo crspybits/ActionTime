32d1450af7df9b3a4ffff954dee1e71ae5f22f12
Christopher Prince <chris@SpasticMuffin.biz>
2020-02-21 11:04:13 -0700

    Update for gofmt standards.

--- action_times.go
+++ action_times.go
@@ -3,38 +3,39 @@ package actiontimes
 import "encoding/json"
 import "errors"
 import "sync"
+
 // import "fmt" // for debugging
 
 // An action name and time parsed from JSON in input data.
 // Using pointers in order to detect missing data
 type inputaction struct {
-  // names of fields must be those as given as they reflect keys in JSON input.
-  // The field names must be uppercased so deserialization will find them in input.
-  Action *string // action name
-  Time *float32 // action duration
+	// names of fields must be those as given as they reflect keys in JSON input.
+	// The field names must be uppercased so deserialization will find them in input.
+	Action *string  // action name
+	Time   *float32 // action duration
 }
 
 // An action name and average duration used in JSON in output data.
 type OutputAction struct {
-  Action string `json:"action"` // action name
-  Avg float32 `json:"avg"` // average action duration
+	Action string  `json:"action"` // action name
+	Avg    float32 `json:"avg"`    // average action duration
 }
 
 // I'm going to track an incremental running average for each action.
 // In this way, I won't have to keep track of all times for each action which could
 // amount to a lot of data over time.
 type current_average struct {
-  average float32
-  n int
+	average float32
+	n       int
 }
 
 // Add next value into the incremental average
 func (r *current_average) addValue(value float32) {
-  // https://math.stackexchange.com/questions/106700/incremental-averageing
-  r.n += 1
-  r.average = r.average + (value - r.average)/float32(r.n)
+	// https://math.stackexchange.com/questions/106700/incremental-averageing
+	r.n += 1
+	r.average = r.average + (value-r.average)/float32(r.n)
 
-  // fmt.Printf("addValue: average[%f]\n", r.average)
+	// fmt.Printf("addValue: average[%f]\n", r.average)
 }
 
 // I'm going to keep the data for the incremental averages in a map from action name
@@ -51,28 +52,28 @@ var mutex = &sync.Mutex{}
 // If the input JSON cannot be parsed, or doesn't contain the expected key/values,
 // this returns an error. No error is returrned if extra key/values are in the string.
 func AddAction(s string) error {
-  var time inputaction
-  err := json.Unmarshal([]byte(s), &time)
-  if err != nil {
-    return err
-  }
+	var time inputaction
+	err := json.Unmarshal([]byte(s), &time)
+	if err != nil {
+		return err
+	}
 
-  if time.Action == nil || time.Time == nil {
-    return errors.New("Missing required field in JSON")
-  }
+	if time.Action == nil || time.Time == nil {
+		return errors.New("Missing required field in JSON")
+	}
 
-  mutex.Lock()
-  _, exists := averages[*time.Action]
-  if !exists {
-    averages[*time.Action] = new(current_average)
-  }
+	mutex.Lock()
+	_, exists := averages[*time.Action]
+	if !exists {
+		averages[*time.Action] = new(current_average)
+	}
 
-  averages[*time.Action].addValue(*time.Time)
-  mutex.Unlock()
+	averages[*time.Action].addValue(*time.Time)
+	mutex.Unlock()
 
-  // fmt.Printf("AddAction: key[%s] value[%f]\n", *time.Action, *time.Time)
+	// fmt.Printf("AddAction: key[%s] value[%f]\n", *time.Action, *time.Time)
 
-  return nil // no error
+	return nil // no error
 }
 
 // Returns a possibly emmpty JSON array with elements of the form:
@@ -80,36 +81,36 @@ func AddAction(s string) error {
 // giving the average time for each action that has been provided to the AddAction function
 // Returns an empty string if there was an error.
 func GetStats() string {
-  var results = []OutputAction{}
+	var results = []OutputAction{}
 
-  // fmt.Printf("Number of actions: %d\n", len(averages))
+	// fmt.Printf("Number of actions: %d\n", len(averages))
 
-  mutex.Lock()
-  for action, average := range averages {
-    // fmt.Printf("key[%s] value[%f]\n", action, average.average)
+	mutex.Lock()
+	for action, average := range averages {
+		// fmt.Printf("key[%s] value[%f]\n", action, average.average)
 
-    // Passing &(action) directly into the ActionTime struct results in a bug. I think it's a string
-    // allocation issue. Instead, do it this way:
-    // var name = action
+		// Passing &(action) directly into the ActionTime struct results in a bug. I think it's a string
+		// allocation issue. Instead, do it this way:
+		// var name = action
 
-    next := OutputAction{action, average.average}
-    results = append(results, next)
-  }
-  mutex.Unlock()
+		next := OutputAction{action, average.average}
+		results = append(results, next)
+	}
+	mutex.Unlock()
 
-  bytes, err := json.Marshal(results)
-  if err != nil {
-    return ""
-  }
+	bytes, err := json.Marshal(results)
+	if err != nil {
+		return ""
+	}
 
-  return string(bytes)
+	return string(bytes)
 }
 
 // Resets averages stored in the library.
 // This was not in the requirements but was needed for testing. It may also
 // be useful in a practical application.
 func Reset() {
-  mutex.Lock()
-  averages = make(map[string]*current_average)
-  mutex.Unlock()
+	mutex.Lock()
+	averages = make(map[string]*current_average)
+	mutex.Unlock()
 }
